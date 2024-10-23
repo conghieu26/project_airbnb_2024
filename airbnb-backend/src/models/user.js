250a2b090dd69aad20e1270797d160b3e619e9b1
conghieu26 <hieunc00@gmail.com>
2024-10-23 14:13:29 +0700

    Change user model

--- airbnb-backend/src/models/user.js
+++ airbnb-backend/src/models/user.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+  _id: { type: Number },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   phone: { type: String },
-  birthday: { type: Date }, // Assuming you want to store birthdays
-  gender: { type: String, enum: ["male", "female", "other"] }, // Or any other options
-  role: { type: String, enum: ["user", "admin"], default: "user" },
+  birthday: { type: String },
+  gender: { type: Boolean },
+  role: { type: String },
 });
 
 export default mongoose.model("User", userSchema);
