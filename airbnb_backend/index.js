3de9fa65b1967bca983c334cfd1c51ec8997e667
conghieu26 <hieunc00@gmail.com>
2024-10-23 16:20:13 +0700

    Fontend enviroment

---
+++ airbnb_backend/index.js
@@ -0,0 +1,8 @@
+import * as dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
