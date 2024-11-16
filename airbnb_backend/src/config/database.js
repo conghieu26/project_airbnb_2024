35c8e8835aa86f20fa54686e1878a835f1d32732
conghieu26 <hieunc00@gmail.com>
2024-11-16 08:30:25 +0700

    Update backend enviroment 2

--- airbnb_backend/src/config/database.js
+++ airbnb_backend/src/config/database.js
@@ -1,16 +1,6 @@
-const mongoose = require("mongoose");
+import { Sequelize } from "sequelize";
 
-const uri =
-  "mongodb+srv://Hieu:Hieu2612@airbnb.pxszq.mongodb.net/?retryWrites=true&w=majority";
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(uri);
-    console.log("MongoDB connected!!");
-  } catch (err) {
-    console.error("Failed to connect to MongoDB:", err);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
+export const db = new Sequelize("travel", "root", "", {
+  dialect: "mysql",
+  host: "localhost",
+});
